02c48a3eea6a8a18d2037b0437c9ffac7e87ad47
yaoyu bao <yaobao5354@gmail.com>
2019-09-26 20:18:20 +0000

    testing list

--- lib/yang_policy/cli.rb
+++ lib/yang_policy/cli.rb
@@ -1,6 +1,12 @@
 class YangPolicy::CLI
+
+
   def call
     puts "Welcome to Yang's policies"
-    binding.pry
+    puts "please enter the topic number you will like to explore"
+    array = ["ECONOMY/JOBS & LABOR", "DEMOCRACY/ GOVERNANCE"]
+    array.each.with_index do |item, index|
+      puts "#{index+1}. #{item.gsub(/\s+/,"_").to_sym}"
+    end
   end
 end
